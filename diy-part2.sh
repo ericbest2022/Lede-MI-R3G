3370e789c3043a95ff358b87572d5fcb319afc07
ericbest2022 <wenlong2022@qq.com>
2025-11-15 13:05:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,10 +34,10 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名
-sed -i 's/LEDE/MIR3G-iStoreOS/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/MIR3G-iStoreOS/g' package/base-files/files/etc/init.d/system
+sed -i 's/LEDE/MIR3G/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/MIR3G/g' package/base-files/files/etc/init.d/system
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/LEDE/MIR3G-iStoreOS/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/LEDE/MIR3G/g' package/lean/default-settings/files/zzz-default-settings
 
 # 替换 banner
 curl -o package/base-files/files/etc/banner https://raw.githubusercontent.com/istoreos/istoreos/refs/heads/istoreos-24.10/package/base-files/files/etc/banner
